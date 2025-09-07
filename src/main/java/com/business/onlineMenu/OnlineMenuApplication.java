84b025cd8c41b7d7b070e1721ea63ce870b37368
tejTarunC <tejtharun625@gmail.com>
2025-09-07 12:16:08 +0530

    added health checks configuration and postgres properties

--- src/main/java/com/business/onlineMenu/OnlineMenuApplication.java
+++ src/main/java/com/business/onlineMenu/OnlineMenuApplication.java
@@ -2,12 +2,15 @@ package com.business.onlineMenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class OnlineMenuApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineMenuApplication.class, args);
+		SpringApplication app = new SpringApplication(OnlineMenuApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
 	}
 
 }
