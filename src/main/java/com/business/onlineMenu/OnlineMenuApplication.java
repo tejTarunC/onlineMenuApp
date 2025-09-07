e5823f61739804e13b2890da0870e1af6bb64118
TejTharun <tejtharun625@gmail.com>
2025-09-07 12:10:00 +0530

    inital setup

---
+++ src/main/java/com/business/onlineMenu/OnlineMenuApplication.java
@@ -0,0 +1,13 @@
+package com.business.onlineMenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMenuApplication.class, args);
+	}
+
+}
