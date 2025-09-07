8af6347071e442e19d6be9a1a7becf00975134a5
tejTarunC <tejtharun625@gmail.com>
2025-09-07 22:27:59 +0530

    added component scan

--- src/main/java/com/business/onlineMenu/OnlineMenuApplication.java
+++ src/main/java/com/business/onlineMenu/OnlineMenuApplication.java
@@ -3,8 +3,15 @@ package com.business.onlineMenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(
+  basePackages = {
+    "com.business.onlineMenu.api",
+    "com.business.onlineMenu.repository",
+    "com.business.onlineMenu.model"
+})
 public class OnlineMenuApplication {
 
   public static void main(String[] args) {
